199c47266d15e2f27896d49c5b070b4defc90658
Rodrigo-Martinss <jrodrigo4321@hotmail.com>
2023-04-12 23:41:06 -0300

    casa 11042023

--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
-const  Sequelize = require('sequelize');
+// const  Sequelize = require('sequelize');
 
-const database = new Sequelize('g2server', 'root', '123456', {
-    host:'localhost',
-    dialect: 'mysql',
-    port:'3306'
-});
+// const database = new Sequelize('g2server', 'root', '123456', {
+//     host:'localhost',
+//     dialect: 'mysql',
+//     port:'3306'
+// });
 
 
-database.authenticate()
-.then(() => {
-    console.log("Conectado ao Banco de Dados");
-}).catch(() => {
-    console.log("Não foi possível se conectar com o Banco de Dados");
-});
+// database.authenticate()
+// .then(() => {
+//     console.log("Conectado ao Banco de Dados");
+// }).catch(() => {
+//     console.log("Não foi possível se conectar com o Banco de Dados");
+// });
 
-module.exports = database;
+// module.exports = database;
